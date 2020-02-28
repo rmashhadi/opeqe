c62d92e546226f8ff550d4fab06fc398a0e49b56
rmashhadi <rme1383@gmail.com>
2020-02-28 13:32:04 +0330

    Updates

---
+++ precache-manifest.0c58cef0e5bcdc4436ff26ab39c4714b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369925a32f6f7346ed1804d07951cfcc",
+    "url": "/opeqe/index.html"
+  },
+  {
+    "revision": "e3f49a8e68559aa22e5d",
+    "url": "/opeqe/static/css/2.0bbf5521.chunk.css"
+  },
+  {
+    "revision": "e28090827ad835d0c726",
+    "url": "/opeqe/static/css/main.ca15b657.chunk.css"
+  },
+  {
+    "revision": "e3f49a8e68559aa22e5d",
+    "url": "/opeqe/static/js/2.ddc896d0.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/opeqe/static/js/2.ddc896d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e28090827ad835d0c726",
+    "url": "/opeqe/static/js/main.f8705ddc.chunk.js"
+  },
+  {
+    "revision": "0054e240940b07ef49f3",
+    "url": "/opeqe/static/js/runtime-main.b59637d5.js"
+  },
+  {
+    "revision": "d83f8e8cf2f874f66e9ac0c56269659e",
+    "url": "/opeqe/static/media/American1.d83f8e8c.jpg"
+  },
+  {
+    "revision": "6583c345ed02c7bdf751d040e7a90852",
+    "url": "/opeqe/static/media/American2.6583c345.jpg"
+  },
+  {
+    "revision": "e77bcc3090aa30f2864ebb7d0455f3ca",
+    "url": "/opeqe/static/media/American3.e77bcc30.jpg"
+  },
+  {
+    "revision": "8273868c0a7345e149065fe596f60aea",
+    "url": "/opeqe/static/media/American4.8273868c.jpg"
+  },
+  {
+    "revision": "9428800672dce1d8d52d95eabc8fc4ba",
+    "url": "/opeqe/static/media/American5.94288006.jpg"
+  },
+  {
+    "revision": "f6c63b42157187d0c9e5e6bfb0edbb71",
+    "url": "/opeqe/static/media/American6.f6c63b42.jpg"
+  },
+  {
+    "revision": "90133cef76c72937d2c6686cd98a5ac2",
+    "url": "/opeqe/static/media/background.90133cef.jpg"
+  },
+  {
+    "revision": "8384a07c027211d4a5b6baed99d57a62",
+    "url": "/opeqe/static/media/logo.8384a07c.png"
+  },
+  {
+    "revision": "9d78e988f936240fc65cd76bca58982c",
+    "url": "/opeqe/static/media/logo2.9d78e988.png"
+  },
+  {
+    "revision": "26e8ec00f637aacfb9e08f774f8236b1",
+    "url": "/opeqe/static/media/salad1.26e8ec00.jpg"
+  },
+  {
+    "revision": "b5524aad81abde38754dc388f165e253",
+    "url": "/opeqe/static/media/salad2.b5524aad.jpg"
+  },
+  {
+    "revision": "ab1e76c8fc06afd8b584d1c890fc6032",
+    "url": "/opeqe/static/media/salad3.ab1e76c8.jpg"
+  },
+  {
+    "revision": "a0050cce8bf2ea227b4512f536bba0f2",
+    "url": "/opeqe/static/media/salad4.a0050cce.jpg"
+  },
+  {
+    "revision": "93b8a4042bfe01c5b55c4e7fa41f795e",
+    "url": "/opeqe/static/media/salad5.93b8a404.jpg"
+  },
+  {
+    "revision": "36c0f36a1c92af5daa05988d3b7abe90",
+    "url": "/opeqe/static/media/salad6.36c0f36a.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/opeqe/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/opeqe/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/opeqe/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/opeqe/static/media/slick.f97e3bbf.svg"
+  }
+]);
